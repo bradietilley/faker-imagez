3bc93547cc0c758cddf064118845c49a44680892
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2024-04-13 17:55:25 +0800

    more comprehensive test

--- tests/Unit/CombineImagezTest.php
+++ tests/Unit/CombineImagezTest.php
@@ -19,5 +19,21 @@ test('you can combine multiple imagez generators', function () {
      * Combine using CombineImagez
      */
     $generator = CombineImagez::make(catSamplez(), dogSamplez(), foodSamplez());
-    expect($generator)->all()->toBe(array_merge(expectCats(), expectDogs(), expectFood()));
+    expect($generator)->all()->toBe($expectAll = array_merge(expectCats(), expectDogs(), expectFood()));
+
+    /**
+     * Can fetch all paths from all 3 sources but in a random order
+     */
+    $all = [];
+
+    while (count($all) < count($expectAll)) {
+        $all[] = $generator->path();
+    }
+
+    expect($all)->not->toBe($expectAll);
+
+    $actual = collect($all)->sort()->values()->all();
+    $expect = collect($expectAll)->sort()->values()->all();
+
+    expect($actual)->toBe($expect);
 });
